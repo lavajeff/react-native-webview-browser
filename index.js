ed8c93a24611aaeb404b6e82acfd66dc88c260cf
Jeffrey Chai <jeffrey@lavax.co>
2018-10-24 15:04:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+import React, { Component } from "react";
+import { View, WebView } from "react-native";
+import PropTypes from "prop-types";
+
+const WEBVIEW_REF = "webview";
+
+class Webbrowser extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View
+        style={[
+          this.props.backgroundColor && {
+            backgroundColor: this.props.backgroundColor
+          }
+        ]}
+      >
+        <WebView
+          ref={WEBVIEW_REF}
+          automaticallyAdjustContentInsets={false}
+          style={styles.webView}
+          source={{ uri: this.props.url }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          decelerationRate="normal"
+          onNavigationStateChange={this.onNavigationStateChange}
+          onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
+          startInLoadingState={true}
+          scalesPageToFit={this.state.scalesPageToFit}
+          onLoad={() => this.refs[WEBVIEW_REF].postMessage(this.props.cookie)}
+          {...(this.props.jsCode
+            ? { injectedJavaScript: this.props.jsCode }
+            : {})}
+          {...this.props.webviewProps}
+        />
+      </View>
+    );
+  }
+}
+
+Webbrowser.propTypes = {
+  url: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  jsCode: PropTypes.string,
+  cookie: PropTypes.string,
+  webviewProps: PropTypes.object
+};
+
+Webbrowser.defaultProps = {
+  url: "",
+  jsCode: null,
+  cookie: "",
+  webviewProps: {}
+};
+
+export default Webbrowser;
